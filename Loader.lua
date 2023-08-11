7bf5931fb1277230c0c8ff0179c3f4b6ee39eaf0
Legacy <100034435+EXFTB@users.noreply.github.com>
2023-08-11 20:57:51 +0200

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,26 @@
+if (not game:IsLoaded()) then
+    game.Loaded:Wait();
+end
+
+local function LoadScript(Script)
+    return loadstring(game:HttpGet(Script..".lua"))()
+end
+
+local function GetGameInformation()
+    for Id, Information in pairs(Avex.Games) do
+        if tostring(game.PlaceId) == Id then
+            return Information
+        end
+    end 
+end
+
+getgenv().Avex = {
+    Source = "https://raw.githubusercontent.com/EXFTB/Avex/main/",
+    Games = {
+        ["8343259840"] = {Name = "Criminality"          Script = "Games/CR"  },
+        ["2262441883"] = {Name = "Electric State"       Script = "Games/ES"   }
+    }
+}
+
+Avex.Game = GetGameInformation()
+LoadScript(Avex.Game.Script)
